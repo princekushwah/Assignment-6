0a87c5f253baf510e4ba4327a7e71bfab40deadc
Ashish <118122681+princekushwah@users.noreply.github.com>
2022-12-07 10:37:06 +0530

    Update 8-Check number prime or not.c

--- 8-Check number prime or not.c
+++ 8-Check number prime or not.c
@@ -1,15 +1,24 @@
 #include<stdio.h>
 int main()
 {
-    int i,n,ans;
+    int i,n,ans=0;
     printf("Enter a number : ");
     scanf("%d",&n);
+    if(n==0||n==i)
+        ans=1;
     for(i=2;i<=n/2;i++)
-        ans=n%i;
+    {
+        if(n%i==0)
+         {
+         ans=1;
+        break;
+        }
+    }
+
         if(ans==0)
-            printf("Not a prime number : ");
+            printf("prime number : ");
         else
-            printf("This is prime number :");
+            printf("Not prime number :");
 
 
     return 0;
